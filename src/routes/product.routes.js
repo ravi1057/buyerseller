4755832e61d0c7ddd4021e1592b4c25b0b5b2311
ravi p <ravi105763@gmail.com>
2024-09-20 22:41:08 +0530

    add product model

---
+++ src/routes/product.routes.js
@@ -0,0 +1,80 @@
+const mongoose = require("mongoose");
+
+const productSchema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: [true, "Please Enter product Name"],
+      trim: true,
+    },
+    description: {
+      type: String,
+      required: [true, "Please Enter product Description"],
+    },
+    price: {
+      type: Number,
+      require: [true, "Please Enter product price"],
+      maxLength: [8, "Price cannot execeed characters"],
+    },
+    ratings: {
+      type: Number,
+      default: 0,
+    },
+    images: [
+      {
+        public_id: {
+          type: String,
+          required: true,
+        },
+        url: {
+          type: String,
+          required: true,
+        },
+      },
+    ],
+    category: {
+      type: String,
+      required: [true, "Please Enter Product Price"],
+    },
+    stock: {
+      type: Number,
+      required: [true, "Please Enter Product Stock"],
+      maxLength: [4, "stock canot execeed 4 characters"],
+      default: 1,
+    },
+    numOfReviews: {
+      type: Number,
+      default: 0,
+    },
+    reviews: [
+      {
+        user: {
+          type: mongoose.Schema.ObjectId,
+          ref: "User",
+          required: true,
+        },
+        name: {
+          type: String,
+          required: true,
+        },
+        rating: {
+          type: Number,
+          required: true,
+        },
+        comment: {
+          type: String,
+          required: true,
+        },
+      },
+    ],
+    seller: {
+      type: mongoose.Schema.ObjectId,
+      ref: "User",
+      required: true,
+    },
+  },
+
+  { timestamps: true }
+);
+
+module.exports = mongoose.model("Product", productSchema);
